4db4c7cfb07cd9b9501be15f931dc57b9dad3ec8
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-02 20:02:07 -0400

    starter

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div id="intro">
+      <h1>My Portfolio</h1>
+      <p>A showcase of my projects and my abilities.</p>
+    </div>
+  );
+}
+
+export default Main;
