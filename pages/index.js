d40b3920b5f6fb29e0a58e9910802f1440943d41
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-03 12:27:31 -0400

    add avatar image and set flex for the hero section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Head from "next/head";
 import { BsFillMoonStarsFill, BsGithub } from "react-icons/bs";
 import { AiOutlineArrowDown } from "react-icons/ai";
+import codingGirl from "../public/coding_girl.svg";
+import avatar from "../public/me2.png";
 
 export default function Home() {
   return (
@@ -16,7 +18,7 @@ export default function Home() {
       <main className="bg-slate-100 px-6 md:px-10">
         <section className="min-h-screen max-w-3xl m-auto">
           {/* navbar */}
-          <nav className="py-10 mb-12 flex justify-between text-gray-600">
+          <nav className="py-10 mb-8 flex justify-between text-gray-600">
             <h1
               className="text-xl md:text-2xl font-spectral font-bold  uppercase
             "
@@ -44,17 +46,37 @@ export default function Home() {
             </ul>
           </nav>
           {/*Hero section  */}
-          <div className="text-center p-10">
-            <h2 className="text-4xl font-extrabold md:text-6xl py-2  text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-green-600">
-              Hi, my name is{" "}
-              <span className=" block md:inline">Dinara Idrissova</span>
-            </h2>
-            <h3 className="text-2xl py-2 text-gray-700">I'm a Web Developer</h3>
-            <p className="text-md py-3 md:py-5  leading-8 text-gray-700">
-              Let me demonstrate my skills and projects through a showcase.
-            </p>
-            <div className="mx-auto mt-8 flex justify-center items-center bg-gradient-to-b from-teal-400 to-green-600 rounded-full w-12 h-12 cursor-pointer ">
-              <AiOutlineArrowDown className="text-white text-2xl " />
+
+          {/* <div className="relative mx-auto w-40 h-40">
+            <Image src={codingGirl} />
+          </div> */}
+          <div className="flex flex-col lg:flex-row-reverse items-start">
+            <div
+              style={{ position: "relative" }}
+              className=" overflow-hidden mx-auto bg-gradient-to-br from-green-400  to-white rounded-full w-56 h-56 shadow-lg lg:mt-6"
+            >
+              <Image
+                alt="Picture of the author"
+                src={avatar}
+                fill
+                style={{ objectFit: "scale-down" }}
+              />
+            </div>
+
+            <div className="text-center p-10">
+              <h2 className="text-3xl font-extrabold  py-2  text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-green-600">
+                Hi, my name is{" "}
+                <span className=" block md:inline">Dinara Idrissova</span>
+              </h2>
+              <h3 className="text-2xl py-2 text-gray-700">
+                I'm a Web Developer
+              </h3>
+              <p className="text-md py-3 md:py-5  leading-8 text-gray-700">
+                Let me demonstrate my skills and projects through a showcase.
+              </p>
+              <div className="mx-auto mt-8 flex justify-center items-center bg-gradient-to-b from-teal-400 to-green-600 rounded-full w-12 h-12 cursor-pointer ">
+                <AiOutlineArrowDown className="text-white text-2xl " />
+              </div>
             </div>
           </div>
         </section>
