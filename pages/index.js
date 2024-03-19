62aa4b45f61ee7b2281151b729b6dd9f7cd0482a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-03-18 22:00:16 -0400

    minor color adjustments

--- pages/index.js
+++ pages/index.js
@@ -43,21 +43,21 @@ export default function Home() {
           content="Dinara Idrissova -a front-end developer, proficient in  JS, CSS, HTML as well as modern front-end frameworks like React.js and NextJS, skilled in building dynamic and responsive user interfaces with a modern design aesthetic. "
         />
       </Head>
-      <main className="bg-slate-50 dark:bg-gray-700 px-6 relative">
+      <main className="bg-[#d6e6da] md:pt-5 dark:bg-gray-700 px-6 relative">
         <section
           id="hero"
-          className="min-h-screen  max-w-5xl m-auto md:px-10 lg:px-16 "
+          className=" min-h-screen flex flex-col   max-w-5xl m-auto  md:px-10 lg:px-16 "
         >
           {/* navbar */}
-          <nav className="lg:px-8 py-8  lg:mb-8 flex justify-between text-gray-600">
+          <nav className=" py-8  lg:mb-8 flex justify-between text-gray-600">
             <h1
-              className="text-xl lg:text-2xl dark:text-white font-spectral font-bold  uppercase
+              className="text-xl lg:text-4xl dark:text-white font-spectral font-bold  uppercase
             "
             >
               Dinara Idrissova
             </h1>
 
-            <ul className="flex items-center gap-3 lg:gap-8">
+            <ul className="flex items-center gap-3 lg:gap-8 ">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() =>
@@ -73,7 +73,7 @@ export default function Home() {
                     href="Dinara_Idrissova_Resume.pdf"
                     target="_blank"
                     rel="noreferrer"
-                    className="text-white text-sm "
+                    className="text-white text-xl lowercase"
                   >
                     Resume
                   </a>
@@ -82,66 +82,73 @@ export default function Home() {
             </ul>
           </nav>
 
-          <div className="flex items-center justify-between flex-col lg:flex-row-reverse   xl:mt-20">
-            <div
-              className=" overflow-hidden lg:mr-8 rounded-full relative w-48 h-48
-              md:w-52 md:h-52 lg:w-64 lg:h-64  bg-avatar_bg bg-cover border-2 border-gray-400"
-            >
-              <Image
-                alt="Dinara Idrissova"
-                src={avatar}
-                fill
-                sizes="(max-width: 300px) 100vw"
-                priority
-                style={{ objectFit: "cover" }}
-                placeholder="blur"
-              />
-            </div>
+          <div className="flex flex-1 flex-col items-center justify-between h-full  py-5 mb-5">
+            <div className="flex   items-center justify-center my-10 flex-col lg:flex-row-reverse   ">
+              <div
+                className=" overflow-hidden lg:mr-8 rounded-full relative w-48 h-48
+              md:w-52 md:h-52 lg:w-72 lg:h-64  bg-avatar_bg border-2 border-teal-300 shadow-xl"
+              >
+                <Image
+                  alt="Dinara Idrissova"
+                  src={avatar}
+                  fill
+                  sizes="(max-width: 300px) 100vw"
+                  priority
+                  style={{ objectFit: "cover" }}
+                  placeholder="blur"
+                />
+              </div>
 
-            <div className="text-center lg:text-start   p-8  lg:transform lg:translate-y-10">
-              <h2 className="text-3xl lg:text-5xl font-extrabold  py-2  text-transparent bg-clip-text bg-gradient-to-t from-teal-600 to-teal-400">
-                Hi, I'm Dinara
-              </h2>
-              <h3 className="text-2xl py-2 text-gray-700 dark:text-white">
-                I'm a Web Developer
-              </h3>
-              <p className="text-md py-3 md:py-5  leading-8 text-gray-700 dark:text-white">
-                Let me demonstrate my skills and projects through the showcase.
-              </p>
+              <div className="text-center lg:text-start  p-8   ">
+                <h2 className="text-3xl lg:text-5xl font-extrabold  py-2  text-transparent bg-clip-text bg-gradient-to-t from-teal-600 to-teal-400">
+                  Hi, I'm Dinara
+                </h2>
+                <h3 className="text-3xl py-2 text-gray-700 dark:text-white">
+                  I'm a Web Developer
+                </h3>
+                <p className="text-2xl py-3 md:py-5  leading-8 text-gray-600 dark:text-white">
+                  Let me demonstrate my skills and projects through the
+                  showcase.
+                </p>
+              </div>
+            </div>
+            <div>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy
+                smooth
+                offset={0}
+                duration={1500}
+                className="mx-auto mb-12  flex justify-center items-center bg-gradient-to-b from-teal-600 to-teal-500 rounded-full w-14 h-14 cursor-pointer hover:scale-110   ease-in-out duration-150 transition-all shadow-md "
+              >
+                <AiOutlineArrowDown className="text-white text-3xl " />
+              </Link>
             </div>
           </div>
-          <Link
-            activeClass="active"
-            to="projects"
-            spy
-            smooth
-            offset={0}
-            duration={1500}
-            className="mx-auto mb-4 lg:my-20 flex justify-center items-center bg-gradient-to-b from-teal-600 to-teal-500 rounded-full w-12 h-12 cursor-pointer hover:scale-105 ease-in-out duration-150 transition-all shadow-md "
-          >
-            <AiOutlineArrowDown className="text-white text-2xl " />
-          </Link>
         </section>
 
         <section id="projects" className="pb-10" title="projects">
+          <Cards />
+        </section>
+
+        <div className="">
           {backToTop && (
             <div
               className="
-            fixed bottom-20 right-10 lg:right-20 xl:right-28 ml-auto   bg-gradient-to-b  from-teal-600 to-teal-500  rounded-full w-9 h-9 cursor-pointer hover:scale-110 ease-in-out duration-150 transition-all shadow-md z-50"
+              fixed bottom-[60px] md:bottom-28 2xl:bottom-80 2xl:right-64 right-10 text-3xl 2xl:text-4xl cursor-pointer  hover:scale-110 ease-in-out duration-150 transition-all shadow-md mb-7 md:mb-0 bg-teal-500 p-2 w-12 h-12 rounded-md "
             >
               <button
                 onClick={scrollUp}
                 className="flex items-center justify-center h-full w-full"
               >
-                <AiOutlineArrowUp className="text-white text-lg " />
+                <AiOutlineArrowUp className="text-white text-2xl " />
               </button>
             </div>
           )}
-
-          <Cards />
-        </section>
+        </div>
         <div
-          className="fixed bottom-[30px] md:bottom-14 2xl:bottom-40 2xl:right-64 right-10 text-3xl 2xl:text-4xl cursor-pointer  hover:scale-110 ease-in-out duration-150 transition-all shadow-md  bg-pink-400 p-2  rounded-md
+          className="fixed bottom-[30px] md:bottom-14 2xl:bottom-40 2xl:right-64 right-10 text-3xl 2xl:text-4xl cursor-pointer  hover:scale-110 ease-in-out duration-150 transition-all shadow-md  bg-teal-500 p-2  rounded-md 
         "
         >
           <a href="mailto:dina.b.idrissova@gmail.com" target="_blank">
